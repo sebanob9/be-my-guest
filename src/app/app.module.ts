bb0da0e3113c9590c7982780708301b2c382582c
unknown <sebanob9@gmail.com>
2019-12-02 19:00:24 +0100

    creado componente slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
