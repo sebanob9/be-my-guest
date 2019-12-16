1e5890158edb6e14d74bb488b0f2191f3913d016
unknown <sebanob9@gmail.com>
2019-12-17 00:38:42 +0100

    corrección thumbnail galeria de fotos primera maquetacion listado invitados creacion pagina de contacto cambio de estilos active - enlaces

--- src/app/wedding/components/guest-sort/guest-sort.component.ts
+++ src/app/wedding/components/guest-sort/guest-sort.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-guest-sort',
   templateUrl: './guest-sort.component.html',
-  styleUrls: ['./guest-sort.component.css']
+  styleUrls: ['./guest-sort.component.scss']
 })
 export class GuestSortComponent implements OnInit {
 
